986ec28158a724311683262f0f617f406f48dd2a
Alexander Link <93796836+LinkAlexander@users.noreply.github.com>
2023-12-08 12:59:06 +0100

    Correcting incorrect merge

--- engine/cgimin/gui/BitmapGraphic.cs
+++ engine/cgimin/gui/BitmapGraphic.cs
@@ -91,8 +91,7 @@ namespace cgimin.engine.gui
 
             if (program == -1)
             {
-                program = ShaderCompiler.CreateShaderProgram("cgimin/gui/shader/BitmapFont_VS.glsl", "cgimin/gui/shader/BitmapGraphic_FS.glsl");
-                GL.BindAttribLocation(program, 0, "in_position");
+                program = ShaderCompiler.CreateShaderProgram("cgimin/gui/shader/BitmapFont_VS.glsl", "cgimin/gui/shader/BitmapGraphic_FS.glsl");                GL.BindAttribLocation(program, 0, "in_position");
                 GL.BindAttribLocation(program, 1, "in_uv");
                 GL.LinkProgram(program);
 
--- example_project/Program.cs
+++ example_project/Program.cs
@@ -104,7 +104,7 @@ public class ExampleProject : GameWindow
 
         
         
-        street = new ObjLoaderObject3D("data/objects/spring.obj");
+        street = new ObjLoaderObject3D("data/objects/bigscene.obj");
         //Once the Object is loaded, put it in front of the camera
         int count = 0; 
         street.Transformation *= Matrix4.CreateTranslation(10, -5, -10);
@@ -116,7 +116,7 @@ public class ExampleProject : GameWindow
             
         // Loading the texture
         woodTexture = TextureManager.LoadTexture("data/textures/duck_texture.png");
-        cellshading = TextureManager.LoadTexture("data/textures/spring.png");
+        cellshading = TextureManager.LoadTexture("data/textures/bigscene1.png");
 
         int spriteTexture = TextureManager.LoadTexture("data/textures/sprites.png");
         logoSprite = new BitmapGraphic(spriteTexture, 256, 256, 10, 110, 196, 120);
