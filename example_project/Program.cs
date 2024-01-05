549445af7c1dc3a0e1d5d1a87ec2866848f17ca7
Alexander Link <93796836+LinkAlexander@users.noreply.github.com>
2024-01-05 11:47:42 +0100

    Update Program.cs

--- example_project/Program.cs
+++ example_project/Program.cs
@@ -26,7 +26,7 @@ namespace cgi;
 public class ExampleProject : GameWindow
 {
 
-    private float cameraSpeed = 0.2f;
+    private float cameraSpeed = 0.15f;
 
     private BitmapGraphic logoSprite;
     private BitmapFont bitmapFont;
@@ -36,6 +36,8 @@ public class ExampleProject : GameWindow
     private List<ObjLoaderObject3D> ducks;
     private ObjLoaderObject3D street = null!;
 
+    private int randomDuckNumber;
+    
     // our texture-IDs
     private int woodTexture;
 
@@ -44,7 +46,8 @@ public class ExampleProject : GameWindow
     // Materials
     private SimpleTextureMaterial simpleTextureMaterial;
     private Wobble2Material wobbleMaterial;
-
+    private List<Vector3> duckPositions = new List<Vector3>();
+    Vector3 unreachableposition = new Vector3(99999, 99999, 9999);
     private PickingRay pickingRay;
 
     // Updating the time
@@ -52,7 +55,7 @@ public class ExampleProject : GameWindow
     
     //Gameplay Timer 
     private static float timer; 
-    private readonly  float starttimer = 46;
+    private readonly  float starttimer = 3;
     public ExampleProject(int width, int height, GameWindowSettings gameWindowSettings,
         NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
@@ -88,7 +91,7 @@ public class ExampleProject : GameWindow
 
     void resetCamera()
     {
-        Camera.Transformation = Matrix4.CreateTranslation(0,-15,-10);
+        Camera.Transformation = Matrix4.CreateTranslation(0,-20,-10);
         Camera.Transformation *= Matrix4.CreateRotationX(MathHelper.DegreesToRadians(45));
     }
     protected override void OnLoad()
@@ -98,13 +101,29 @@ public class ExampleProject : GameWindow
         updateTime = 0;
         //Lighting
         Light.SetDirectionalLight(new Vector3(1, 1, 1), new Vector4(0.5f, 0.5f, 0.5f, 1), new Vector4(0.5f, 0.5f, 0.5f, 1),
-            new Vector4(0, 0, 0, 0));
+            new Vector4(0f, 0f, 0f, 1));
         // Initialize Camera
         Camera.Init();
         Camera.SetWidthHeightFov(1920, 1080, 60);
         resetCamera();
         
-        int duckCount = 10;
+        duckPositions.Add(new Vector3(10.3f,3.8f,-17.3f)); //Ente Bank
+        duckPositions.Add(new Vector3(-3,4f,-8f)); //Ente Auto
+        duckPositions.Add(new Vector3(17.5f,5f,5f)); //Ente Brunnen
+        duckPositions.Add(new Vector3(-17.5f,10f,-27f)); //Ente Schornstein
+        duckPositions.Add(new Vector3(-10f,6.5f,12f)); //Ente Baum unten links
+        duckPositions.Add(new Vector3(14.5f,3.5f,-27f)); //Ente Haustür oben rechts
+        duckPositions.Add(new Vector3(-20f,5.5f,-6f)); //Ente Haus Fenster mitte links
+        duckPositions.Add(new Vector3(-10f,5.5f,-15f)); //Ente Laterne hinten links
+        duckPositions.Add(new Vector3(17f,3.5f,12f)); //Ente Horizontale Bank unten rechts
+        duckPositions.Add(new Vector3(0f,2.5f,13f)); //Ente Straße unten
+        duckPositions.Add(new Vector3(0f,3f,-35f)); //Ente Straße oben
+        duckPositions.Add(new Vector3(21f,6f,-5f)); //Ente Garage rechts mitte
+        duckPositions.Add(new Vector3(25f,3.5f,-20f)); //Ente hinter haus oben rechts
+
+        
+        
+        int duckCount = duckPositions.Count;
         // Loading the object
         for (int i = 0; i < duckCount; i++)
         {
@@ -113,16 +132,16 @@ public class ExampleProject : GameWindow
         
         street = new ObjLoaderObject3D("data/objects/szene.obj");
         //Once the Object is loaded, put it in front of the camera
-         
-        street.Transformation *= Matrix4.CreateTranslation(0, -5, -10);
-        int duckPlacementCounter = -2;
+        
+        street.Transformation *= Matrix4.CreateTranslation(0, 0, -10);
+        int duckPlacementCounter = 1;
         foreach (var duck in ducks)
         {
-            duck.Transformation = Matrix4.CreateTranslation(duckPlacementCounter * 5, 0, -5);
-            duck.ScaleInPlace(0.3f);
+            if (duckPlacementCounter > duckCount) duckPlacementCounter = duckCount;
+            duck.Transformation = Matrix4.CreateTranslation(unreachableposition);
             duckPlacementCounter++;
         }
-            
+        placeOneDuck();
         // Loading the texture
         woodTexture = TextureManager.LoadTexture("data/textures/duck_texture.png");
         cellshading = TextureManager.LoadTexture("data/textures/szene-texture2.png");
@@ -140,6 +159,16 @@ public class ExampleProject : GameWindow
         
     }
 
+    protected void placeOneDuck()
+    {
+        Random rnd = new Random();
+        int randomDuckNumber = rnd.Next(ducks.Count-1);
+        ducks[randomDuckNumber].Transformation = Matrix4.CreateTranslation(duckPositions[randomDuckNumber]);
+        ducks[randomDuckNumber].ScaleInPlace(0.1f);
+        ducks[randomDuckNumber].RotateObjectX(rnd.Next(360));
+        ducks[randomDuckNumber].RotateObjectZ(rnd.Next(360));
+    }
+    
     private int score = 0;
     protected void PickDuck()
     {
@@ -152,13 +181,13 @@ public class ExampleProject : GameWindow
         foreach (var duck in ducks)
         {
             if (!duck.RayIntersectsObject(pickingRay)) continue;
-            duck.Transformation *= Matrix4.CreateTranslation(0, -500, 0);
+            duck.Transformation = Matrix4.CreateTranslation(unreachableposition);
             score++;
             cameraSpeed *= 0.9f;
             updateTime = 0;
-
+            placeOneDuck();
         }
-
+        //Console.WriteLine(Camera.Transformation.Inverted().ExtractTranslation());
     }
         
     protected override void OnUpdateFrame(FrameEventArgs e)
@@ -167,29 +196,42 @@ public class ExampleProject : GameWindow
         // updateCounter simply increases
         updateTime += (float)e.Time;
         timer = starttimer - updateTime* score/5;
-        int bound = 25;
-        //Movement of the camera according to the keys pressed, only when within the boundaries
-
-        if (KeyboardState.IsKeyDown(Keys.W) && Camera.Transformation.ExtractTranslation().Z < bound)
-            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,-cameraSpeed,cameraSpeed));
-        if (KeyboardState.IsKeyDown(Keys.S) && -Camera.Transformation.ExtractTranslation().Z < bound)
-            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,cameraSpeed,-cameraSpeed));
-        if (KeyboardState.IsKeyDown(Keys.A) && Camera.Transformation.ExtractTranslation().X < bound)
-            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(cameraSpeed,0,0));
-        if (KeyboardState.IsKeyDown(Keys.D) && -Camera.Transformation.ExtractTranslation().X < bound)
-            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(-cameraSpeed,0,0));
-        
-        // Seitwärts gucken (funktioniert so halb)
-        if (KeyboardState.IsKeyDown(Keys.E) && Camera.Transformation.ExtractTranslation().Y < bound)
-            Camera.Transformation *= Matrix4.CreateRotationY(cameraSpeed+(float)Math.Sin(0.0001f));
-        if (KeyboardState.IsKeyDown(Keys.Q) && Camera.Transformation.ExtractTranslation().Y < bound)
-            Camera.Transformation *= Matrix4.CreateRotationY(-cameraSpeed-(float)Math.Sin(0.0001f));
-
-
         if (timer <= 0)
         {
-            Close();
+            resetCamera();
+            
+            bitmapFont.DrawString("GAME OVER \n " +
+                                  "Score: " + score, -Size.X, -Size.Y, 255, 255, 255, 255);
+               //TODO Label richtig setzen
         }
+        else{
+        
+        int xbound = 28;
+        int zbound = 38;
+        //Movement of the camera according to the keys pressed, only when within the boundaries
+
+        if (KeyboardState.IsKeyDown(Keys.W) && Camera.Transformation.ExtractTranslation().Z < zbound-40)
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,-cameraSpeed,cameraSpeed));
+        if (KeyboardState.IsKeyDown(Keys.S) && -Camera.Transformation.ExtractTranslation().Z < zbound)
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,cameraSpeed,-cameraSpeed));
+        if (KeyboardState.IsKeyDown(Keys.A) && Camera.Transformation.ExtractTranslation().X < xbound)
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(cameraSpeed,0,0));
+        if (KeyboardState.IsKeyDown(Keys.D) && -Camera.Transformation.ExtractTranslation().X < xbound)
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(-cameraSpeed,0,0));
+        
+        if(KeyboardState.IsKeyDown(Keys.Up))
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,0,-cameraSpeed));
+        if(KeyboardState.IsKeyDown(Keys.Down))
+            Camera.Transformation *= Matrix4.CreateTranslation(new Vector3(0,0,cameraSpeed));
+        
+        // Seitwärts gucken (funktioniert so halb)
+//        if (KeyboardState.IsKeyDown(Keys.E) && Camera.Transformation.ExtractTranslation().Y < bound)
+//            Camera.Transformation *= Matrix4.CreateRotationY(cameraSpeed/100);
+//        if (KeyboardState.IsKeyDown(Keys.Q) && Camera.Transformation.ExtractTranslation().Y < bound)
+//            Camera.Transformation *= Matrix4.CreateRotationY(-cameraSpeed/100);
+
+        }
+        
     }
     
     protected override void OnRenderFrame(FrameEventArgs e)
